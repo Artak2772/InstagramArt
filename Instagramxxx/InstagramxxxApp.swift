6a4223d678c4f5d9e0645c7677d8504e6f41948b
Artak2772 <ArtakMheryan@icloud.com>
2023-08-07 17:22:41 +0200

    Initial Commit

---
+++ Instagramxxx/InstagramxxxApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramxxxApp.swift
+//  Instagramxxx
+//
+//  Created by Artak Mheryan on 07.08.23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramxxxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
